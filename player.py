d347515846cd10720af3f2cb37e7e95e085c6402
Adriel Estoller <adrielestoller@gmail.com>
2026-02-08 17:47:40 -0300

    feat: add shots

--- player.py
+++ player.py
@@ -4,8 +4,10 @@ from constants import (
     PLAYER_RADIUS,
     LINE_WIDTH,
     PLAYER_TURN_SPEED,
-    PLAYER_SPEED
+    PLAYER_SPEED,
+    PLAYER_SHOOT_SPEED
 )
+from shot import Shot
 
 
 class Player(CircleShape):
@@ -32,6 +34,11 @@ class Player(CircleShape):
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(
+            self.rotation) * PLAYER_SHOOT_SPEED
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
@@ -46,3 +53,5 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * -1)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
